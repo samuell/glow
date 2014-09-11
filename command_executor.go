fdee0f06d5be155ee929f23ea92fc9aec7d73fa2
Samuel Lampa <samuel.lampa@gmail.com>
2014-09-11 10:09:00 +0200

    Improved code organization and comments

--- command_executor.go
+++ command_executor.go
@@ -23,21 +23,24 @@ type CommandExecutor struct {
 
 func (self *CommandExecutor) Init() {
 	go func() {
-		// Set up things
+		// Set up the command to run
 		cmdParts := strings.Fields(<-self.CommandIn)
 		cmdHead := cmdParts[0]
 		cmdTail := cmdParts[1:len(cmdParts)]
-
 		cmd := exec.Command(cmdHead, cmdTail...)
+
+		// Connect a buffer to the stdout of the command
 		var out bytes.Buffer
 		cmd.Stdout = &out
+		scan := bufio.NewScanner(&out)
 
-		// Execute the actual command
+		// Start executing the command
 		err := cmd.Run()
 		if err != nil {
 			log.Fatal(err)
 		}
-		scan := bufio.NewScanner(&out)
+
+		// Write the command output to the output channel LinesOut
 		for scan.Scan() {
 			self.LinesOut <- append([]byte(nil), scan.Bytes()...)
 		}
